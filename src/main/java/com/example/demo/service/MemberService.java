d103e72ebac4a7606a42c2346bd283741c16f39f
Luna <luvbliny@gmail.com>
2022-06-12 20:57:54 +0900

    Feat: db attached

--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -6,10 +6,12 @@ import com.example.demo.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
 
